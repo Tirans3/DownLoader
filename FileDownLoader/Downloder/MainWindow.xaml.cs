909ac5f3d15dfe511216866c5b17f14da0a844f6
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-22 16:44:22 +0400

    Add clean code

--- FileDownLoader/Downloder/MainWindow.xaml.cs
+++ FileDownLoader/Downloder/MainWindow.xaml.cs
@@ -1,30 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Downloder
 {
-   
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            
         }
 
         string str;
@@ -32,10 +19,10 @@ namespace Downloder
         HttpClient client = new HttpClient();
 
         public bool flag = true;
-       
+
         private async void Download_Click(object sender, RoutedEventArgs e)
         {
-           
+
             Download.IsEnabled = false;
             str = Inputurl.Text;
             if (str == string.Empty || !Uri.IsWellFormedUriString(str, UriKind.RelativeOrAbsolute))
@@ -60,25 +47,25 @@ namespace Downloder
                             {
                                 if (flag == false) return;
                                 if (Bar.Value == 100) Bar.Value = 0;
-                                
+
                                 Bar.Value += 0.001;
 
-                               outtext.Insert(outtext.Length,line);
+                                outtext.Insert(outtext.Length, line);
                             }
 
                             Bar.Value = 100;
 
-                            outbox.Text = outtext.ToString(); 
+                            outbox.Text = outtext.ToString();
 
                         }
 
                     }
                 }
-                catch(HttpRequestException ex )
+                catch (HttpRequestException ex)
                 {
                     outbox.Text = ex.Message;
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     outbox.Text = ex.Message;
                 }
@@ -96,7 +83,7 @@ namespace Downloder
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
             flag = false;
-         
+
         }
     }
 }
